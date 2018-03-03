9f54e3153b41fefc8bd72673661a3f400805f458
Abhishek Ghosh <abhishek.ghosh@copart.com>
2018-03-03 15:45:27 -0800

    🌚 Added placeholder when todo list is empty

--- app/components/TodoList/TodoList.js
+++ app/components/TodoList/TodoList.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import {
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
 
 import TodoItem from '../TodoItem';
 
 const TodoList = props => (
-  <ScrollView style={{ flex: 1, padding: 20, margin: 10 }}>
+  <ScrollView style={styles.container}>
     {props &&
-      props.data &&
-      props.data.map(todo => <TodoItem todo={todo} {...props} key={todo.id} />)}
+    props.data &&
+    Array.isArray(props.data) &&
+    props.data.length > 0 ? (
+      props.data.map(todo => <TodoItem todo={todo} {...props} key={todo.id} />)
+    ) : (
+      <View style={styles.stubStyle}>
+        <Text
+          style={{
+            color: 'black',
+            fontWeight: 'bold',
+            fontStyle: 'italic'
+          }}
+        >
+          Don't you have anything to-do ? 🌚
+        </Text>
+      </View>
+    )}
   </ScrollView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    margin: 10
+  },
+  stubStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 5,
+    borderColor: '#f8bc45',
+    backgroundColor: '#f8bc45',
+    borderWidth: 1.5,
+    marginVertical: 10
+  }
+});
 
 export default TodoList;
